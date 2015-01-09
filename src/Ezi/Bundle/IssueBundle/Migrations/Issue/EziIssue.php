fd36cca3ec4aa98ce2ced62260df90cf10fbd038
ezi <ezi@localhost.localdomain>
2015-01-09 23:18:01 +0200

    Fix entities, make migrations

--- src/Ezi/Bundle/IssueBundle/Migrations/Issue/EziIssue.php
+++ src/Ezi/Bundle/IssueBundle/Migrations/Issue/EziIssue.php
@@ -19,21 +19,32 @@ use Ezi\Bundle\IssueBundle\Migrations\Issue\EziIssueResolution;
 
 class EziIssue implements Migration
 {
-    const TABLE_NAME = 'ezi_issue';
+    const TABLE_NAME  = 'ezi_issue';
+    const PRIMARY_KEY = 'id';
+
+    /**
+     * @var Schema
+     */
+    private $schema;
+
+    /**
+     * @var QueryBag
+     */
+    private $queries;
 
     /**
      * Column array that provides in schema builder
      *
      * @var array
      */
-    protected $issueTableFields = [
+    private $issueTableColumns = [
         ['id',                'integer',  ['autoincrement' => true]             ],
         ['summary',           'string',   ['notnull' => false, 'length' => 255] ],
         ['code',              'string',   ['notnull' => true,  'length' => 32]  ],
         ['description',       'string',   ['notnull' => false, 'length' => 255] ],
-        ['type',              'string',   ['notnull' => true,  'length' => 32]  ],
-        ['priority',          'string',   ['notnull' => true,  'length' => 32]  ],
-        ['resolution',        'string',   ['notnull' => true,  'length' => 32]  ],
+        ['type_id',           'string',   ['notnull' => true,  'length' => 32]  ],
+        ['priority_id',       'string',   ['notnull' => true,  'length' => 32]  ],
+        ['resolution_id',     'string',   ['notnull' => true,  'length' => 32]  ],
         ['status',            'string',   ['notnull' => true,  'length' => 32]  ],
         ['tags',              'string',   ['notnull' => true,  'length' => 32]  ],
         ['reporter_id',       'integer',  ['notnull' => true,  'length' => 32]  ],
@@ -49,12 +60,76 @@ class EziIssue implements Migration
         ['updated_at',        'datetime', ['notnull' => true]                   ],
     ];
 
-    protected $issueTableIndexes = [
-
+    private $issueTableIndexes = [
+        [['summary'],           "EZI_ISSUE_SUMMARY_IDX",           []],
+        [['code'],              "EZI_ISSUE_CODE_IDX",              []],
+        [['descripton'],        "EZI_ISSUE_DESCRIPTION_IDX",       []],
+        [['reporter_id'],       "EZI_ISSUE_STATUS_IDX",            []],
+        [['assignee_id'],       "EZI_ISSUE_ASSIGNEE_ID_IDX",       []],
+        [['assignee_id'],       "EZI_ISSUE_RELATED_ISSUES_ID_IDX", []],
+        [['related_issues_id'], "EZI_ISSUE_RELATED_ISSUES_ID_IDX", []],
+        [['collaborators'],     "EZI_ISSUE_COLLABORATORS_ID_IDX",  []],
+        [['parent_id'],         "EZI_ISSUE_ISSUE_PARENT_ID_IDX",   []],
+        [['child_id'],          "EZI_ISSUE_CHILDREN_ID_IDX",       []],
+        [['workflow_step_id'],  "EZI_ISSUE_WORKFLOW_STEP_ID_IDX",  []],
+        [['notes'],             "EZI_ISSUE_NOTES_IDX",             []]
     ];
 
-    protected $issueForeignKeys = [
-
+    private $issueForeignKeys = [
+        [
+            EziIssueType::TABLE_NAME,
+            ['type_id'],
+            ['id'],
+            ['onDelete' => 'SET NULL']
+        ],
+        [
+            EziIssuePriority::TABLE_NAME,
+            ['priority_id'],
+            ['id'],
+            ['onDelete' => 'SET NULL']
+        ],
+        [
+            EziIssueResolution::TABLE_NAME,
+            ['resolution_id'],
+            ['id'],
+            ['onDelete' => 'SET NULL']
+        ],
+        [
+            'oro_user',
+            ['owner_id'],
+            ['id'],
+            ['onDelete' => 'SET NULL']
+        ],
+        [
+            'orocrm_account',
+            ['related_account_id'],
+            ['id'],
+            ['onDelete' => 'SET NULL']
+        ],
+        [
+            'orocrm_contact',
+            ['related_contact_id'],
+            ['id'],
+            ['onDelete' => 'SET NULL']
+        ],
+        [
+            'oro_user',
+            ['reporter_id'],
+            ['id'],
+            ['onDelete' => 'SET NULL']
+        ],
+        [
+            'oro_workflow_item',
+            ['workflow_item_id'],
+            ['id'],
+            ['onDelete' => 'SET NULL']
+        ],
+        [
+            'oro_workflow_step',
+            ['workflow_step_id'],
+            ['id'],
+            ['onDelete' => 'SET NULL']
+        ]
     ];
 
     /**
@@ -67,48 +142,76 @@ class EziIssue implements Migration
      */
     public function up(Schema $schema, QueryBag $queries)
     {
+        $this->schema  = $schema;
+        $this->queries = $queries;
+
         $this->setup($schema, $queries);
     }
 
-    protected function setup(Schema $schema, QueryBag $queries)
+    protected function setup()
+    {
+        $this->dropTableIfExists($this->schema);
+        $this->createDictionaryTables($this->schema, $this->queries);
+
+        $this->addIssueForeignKeys(
+            $this->addIssueIndexes(
+                $this->addTableStructure(
+                    $this->schema->createTable(self::TABLE_NAME)
+                )
+            )
+        );
+
+    }
+
+    private function dropTableIfExists(Schema $schema)
     {
         if ($schema->hasTable(self::TABLE_NAME)) {
             $schema->dropTable(self::TABLE_NAME);
         }
-
-        $table = $this->createTableStructure(
-            $schema->createTable(self::TABLE_NAME)
-        );
-
-        $this->createDictionaryTables($schema, $queries);
-
     }
 
-    protected function createDictionaryTables(Schema $schema, QueryBag $queries)
+    private function createDictionaryTables()
     {
-        (new EziIssueType())->up($schema, $queries);
-        (new EziIssuePriority())->up($schema, $queries);
-        (new EziIssueResolution())->up($schema, $queries);
+        (new EziIssueType())->up($this->schema, $this->queries);
+        (new EziIssuePriority())->up($this->schema, $this->queries);
+        (new EziIssueResolution())->up($this->schema, $this->queries);
     }
 
-    protected function createTableStructure(Table $table)
+    private function addTableStructure(Table $table)
     {
-        $method = new \ReflectionMethod($table, 'addColumn');
+        return $this->invokeTableMethod($table, 'addColumn', $this->issueTableColumns);
+    }
 
-        foreach($this->issueTableFields as $args) {
+    private function addIssueIndexes(Table $table)
+    {
+        return $this->invokeTableMethod($table, 'addIndex', $this->issueTableIndexes);
+    }
+
+    private function addIssueForeignKeys(Table $table)
+    {
+        $schema   = $this->schema;
+        $callback = function(array $args) use ($schema) {
+            $args[0] = $schema->getTable($args[0]);
+
+            return $args;
+        };
+
+        return $this->invokeTableMethod($table, 'addForeignKeyConstraint', $this->issueForeignKeys, $callback);
+    }
+
+    private function invokeTableMethod(Table $table, $method, array $argsArray, \Closure $redefineArgs = null)
+    {
+        $method = new \ReflectionMethod($table, $method);
+
+        foreach($argsArray as $args) {
+
+            if (!is_null($redefineArgs)) {
+                $args = $redefineArgs($args);
+            }
+
             $method->invokeArgs($table, $args);
         }
 
-        return $table;
-    }
-
-    protected function addIssueIndexes(Table $table)
-    {
-        return $table;
-    }
-
-    protected function addIssueForeighns(Table $table)
-    {
         return $table;
     }
 }
