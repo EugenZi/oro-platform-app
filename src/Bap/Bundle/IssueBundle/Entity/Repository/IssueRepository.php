4d85c35b2e88f6356cebd0d89e4708dc9c4a4164
ezi <ezi@localhost.localdomain>
2015-01-28 13:48:07 +0200

    Fix parameters.yml

--- src/Bap/Bundle/IssueBundle/Entity/Repository/IssueRepository.php
+++ src/Bap/Bundle/IssueBundle/Entity/Repository/IssueRepository.php
@@ -2,6 +2,7 @@
 
 namespace Bap\Bundle\IssueBundle\Entity\Repository;
 
+use Doctrine\ORM\AbstractQuery;
 use Doctrine\ORM\EntityRepository;
 
 /**
@@ -10,10 +11,29 @@ use Doctrine\ORM\EntityRepository;
  */
 class IssueRepository extends  EntityRepository
 {
-    public function getIssuesByStatus()
+    public function getIssuesByStatus($status)
     {
         $em = $this->getEntityManager();
         $qb = $em->createQueryBuilder();
 
+        $qb->select(['COUNT(issue.id) as cnt', 'workflowStep.label'])
+            ->from('OroWorkflowBundle:WorkflowStep', 'workflowStep')
+            ->leftJoin('workflowStep.definition', 'definition')
+            ->leftJoin(
+                'BapIssueBundle:Issue',
+                'issue',
+                'WITH',
+                'issue.workflowStep = workflowStep AND definition.name = :d_name'
+            )
+            ->andWhere('')
+            ->groupBy('workflowStep.label')
+            ->orderBy('cnt')
+            ->setParameter('d_name', 'issue_flow');
+
+        return $qb
+            ->getQuery()
+            ->getResult(
+                AbstractQuery::HYDRATE_SIMPLEOBJECT
+            );
     }
 }
